c21920c6b4badc082134c3310f123bf50741fc62
Zach Caceres (zach.dev) <hello@zach.dev>
2021-09-24 19:13:55 -0600

    sketch out more functions, improve spec, flesh out api

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # snoop
-Like ack, for the DOM
+Like grep or ack... for the DOM
 
-THIS IS A SPEC.
+## Spec
 
-## How to use
+### How to use
 ```js
-snoop('token')
+snoop('token', { asSubstring: true })
 ```
 
 Will return 'token' as a data attribute of any node, as a property on the Window object, in any urls on any tags, and in any JSON that can be found and parsed in the HTML.
 
-1. Must be case insensitive ('TOKEN', 'token', and 'Token' all found).
-
+- Must be case insensitive ('TOKEN', 'token', and 'Token' all found).
+- Optionally finds as a substring (configurable)
+- Shows the result in context and makes it easy to find the reference
--- lib/index.ts
+++ lib/index.ts
@@ -1,30 +1,81 @@
-function searchGlobals(query: string) {}
+interface SnoopOptions {
+  asSubstring: boolean;
+  globals: boolean;
+  scripts: boolean;
+  meta: boolean;
+  body: boolean;
+  images: boolean;
+  cookies: boolean;
+}
 
-function searchHeaderFooterMeta(query: string) {}
 
-function searchJsonLD(query: string) {}
+function formatFoundMessage(element: string, match: string, msg: string | undefined = '') {
+  console.log(`Found match: ${match} in element ${element}. ${msg}`)
+}
 
-function searchBodyText(query: string) {}
+function searchGlobals(query: string) {
+  return traverseObjectFor(window, query);
+  // traverse tree of properties recursively
+  // if found, print message
 
-function searchImages(query: string) {}
+
+}
+
+function traverseObjectFor(obj: object, query: string) {
+
+}
+
+
+function searchScripts(query: string) {
+  const tags = Array.from(document.querySelectorAll('script'));
+}
+
+function searchMeta(query: string) {
+  const tags = Array.from(document.querySelectorAll('meta'));
+
+}
+
+function searchBodyText(query: string) {
+  const body = document.querySelector('body');
+  const text = body ? body.textContent : '';
+
+}
+
+function searchImages(query: string) {
+  const tags = Array.from(document.querySelectorAll('img'));
+}
+
+function searchCookies(query: string) {
+  const text = document.cookie;
+}
 
 // Possibly parallelize via WebWorkers?
 
-function snoop(query: string) {
-  searchGlobals(query);
+function snoop(query: string, options: SnoopOptions = {
+  asSubstring: false,
+  globals: true,
+  scripts: true,
+  meta: true,
+  body: true,
+  images: true,
+  cookies: true,
+}) {
+  if (options.globals) searchGlobals(query);
   console.log("searching globals for", query);
 
-  searchHeaderFooterMeta(query);
-  console.log("searching header, footer, and meta tags for", query);
+  if (options.scripts) searchScripts(query);
+  console.log("searching scripts for", query);
 
-  searchJsonLD(query);
-  console.log("searching json-ld for", query);
+  if (options.meta) searchMeta(query);
+  console.log("searching meta for", query);
 
-  searchBodyText(query);
+  if (options.body) searchBodyText(query);
   console.log("searching body text for", query);
 
-  searchImages(query);
+  if (options.images) searchImages(query);
   console.log("searching images and alt tags for", query);
+
+  if (options.cookies) searchCookies(query);
 }
 
 // Make the function globally available.
